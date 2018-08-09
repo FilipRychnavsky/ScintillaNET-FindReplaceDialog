f195057968c7416d0068ec5280de6a5740354c50
Filip Rychnavsky at CVS mbH <FRychnavsky@cvs.de>
2018-08-09 14:25:46 +0200

    scintilla3, tabPage3

--- Demo/Form1.Designer.cs
+++ Demo/Form1.Designer.cs
@@ -35,10 +35,11 @@
 			this.tabControl1 = new System.Windows.Forms.TabControl();
 			this.tabPage1 = new System.Windows.Forms.TabPage();
 			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.tabPage3 = new System.Windows.Forms.TabPage();
 			this.scintilla2 = new ScintillaNET.Scintilla();
+			this.scintilla3 = new ScintillaNET.Scintilla();
 			this.findAllResultsPanel1 = new ScintillaNET_FindReplaceDialog.FindAllResults.FindAllResultsPanel();
 			this.incrementalSearcher1 = new ScintillaNET_FindReplaceDialog.IncrementalSearcher();
-			this.tabPage3 = new System.Windows.Forms.TabPage();
 			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
 			this.splitContainer1.Panel1.SuspendLayout();
 			this.splitContainer1.Panel2.SuspendLayout();
@@ -110,7 +111,7 @@
 			this.tabPage1.Name = "tabPage1";
 			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
 			this.tabPage1.Size = new System.Drawing.Size(707, 224);
-			this.tabPage1.TabIndex = 0;
+			this.tabPage1.TabIndex = 1;
 			this.tabPage1.Text = "tabPage1";
 			this.tabPage1.UseVisualStyleBackColor = true;
 			// 
@@ -121,9 +122,18 @@
 			this.tabPage2.Name = "tabPage2";
 			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
 			this.tabPage2.Size = new System.Drawing.Size(707, 224);
-			this.tabPage2.TabIndex = 1;
+			this.tabPage2.TabIndex = 2;
 			this.tabPage2.Text = "tabPage2";
 			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// tabPage3
+			// 
+			this.tabPage3.Location = new System.Drawing.Point(4, 22);
+			this.tabPage3.Name = "tabPage3";
+			this.tabPage3.Size = new System.Drawing.Size(707, 224);
+			this.tabPage3.TabIndex = 0;
+			this.tabPage3.Text = "tabPage3";
+			this.tabPage3.UseVisualStyleBackColor = true;
 			// 
 			// scintilla2
 			// 
@@ -137,6 +147,19 @@
 			this.scintilla2.WrapMode = ScintillaNET.WrapMode.Word;
 			this.scintilla2.Enter += new System.EventHandler(this.genericScintilla1_Enter);
 			this.scintilla2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.genericScintilla_KeyDown);
+			// 
+			// scintilla3
+			// 
+			this.scintilla3.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.scintilla3.Location = new System.Drawing.Point(3, 3);
+			this.scintilla3.Name = "scintilla3";
+			this.scintilla3.ScrollWidth = 5001;
+			this.scintilla3.Size = new System.Drawing.Size(701, 218);
+			this.scintilla3.TabIndex = 1;
+			this.scintilla3.Text = resources.GetString("scintilla3.Text");
+			this.scintilla3.WrapMode = ScintillaNET.WrapMode.Word;
+			this.scintilla3.Enter += new System.EventHandler(this.genericScintilla1_Enter);
+			this.scintilla3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.genericScintilla_KeyDown);
 			// 
 			// findAllResultsPanel1
 			// 
@@ -161,15 +184,6 @@
 			this.incrementalSearcher1.Size = new System.Drawing.Size(259, 22);
 			this.incrementalSearcher1.TabIndex = 1;
 			this.incrementalSearcher1.ToolItem = true;
-			// 
-			// tabPage3
-			// 
-			this.tabPage3.Location = new System.Drawing.Point(4, 22);
-			this.tabPage3.Name = "tabPage3";
-			this.tabPage3.Size = new System.Drawing.Size(707, 224);
-			this.tabPage3.TabIndex = 2;
-			this.tabPage3.Text = "tabPage3";
-			this.tabPage3.UseVisualStyleBackColor = true;
 			// 
 			// Form1
 			// 
@@ -196,15 +210,16 @@
 		#endregion
 
 		private ScintillaNET.Scintilla scintilla1;
-        private ScintillaNET_FindReplaceDialog.IncrementalSearcher incrementalSearcher1;
-        private System.Windows.Forms.Button GotoButton;
-        private ScintillaNET_FindReplaceDialog.FindAllResults.FindAllResultsPanel findAllResultsPanel1;
-        private System.Windows.Forms.SplitContainer splitContainer1;
-        private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
-        private ScintillaNET.Scintilla scintilla2;
+		private ScintillaNET_FindReplaceDialog.IncrementalSearcher incrementalSearcher1;
+		private System.Windows.Forms.Button GotoButton;
+		private ScintillaNET_FindReplaceDialog.FindAllResults.FindAllResultsPanel findAllResultsPanel1;
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TabPage tabPage2;
 		private System.Windows.Forms.TabPage tabPage3;
+		private ScintillaNET.Scintilla scintilla2;
+		private ScintillaNET.Scintilla scintilla3;
 	}
 }
 
